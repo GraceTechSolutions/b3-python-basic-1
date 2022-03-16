74a4c2adcfe8163c3e504a8b16dead2a53b4cf29
JRudransh <JRudransh@protonmail.com>
2022-03-16 19:06:34 +0530

    File upload

---
+++ main.py
@@ -0,0 +1,192 @@
+'''
+Hello
+I am Doc String
+Line 2
+-->
+'''
+
+# Line by line ->>
+# ->>>
+# error
+# PVM -> Python
+
+# print('Hello world')
+
+
+
+# PVM Byte code
+
+# C/C++
+
+# TKinter
+
+# Cpython -->> C
+# Jython -> JAVA
+# IronPython -> .net
+# RubyPython -> ruby
+# Anaconda -> ML -> TS, Numpy Pandas
+
+
+# Variables
+# Can contain with a-z A-Z 0-9 _
+# can't start with a number
+# can be started with A-Z a-z _
+# case sensitive
+
+# Not Same
+from curses.ascii import isalpha, isspace
+from lib2to3.pytree import convert
+
+
+a = 10
+A = 20
+
+# a,b = 10, 20
+
+
+# Scopes
+
+# global
+# name = 'Jay'
+
+# local
+def hi():
+    global name
+    name = 'Rahul'
+    print('Hello ' +name)
+
+# hi()
+
+# print(name)
+
+# Built in datatypes
+
+# str
+s1 = 'Hello'
+s2 = "hello"
+s3 = '''Hii'''
+s4 = """hii..."""
+
+# int
+i1 = 1
+i2 = 3
+
+# float
+f1 = 2.1
+f2 = 2.0
+
+# complex
+c1 = 2j
+
+# list
+l1 = [1,2, 'hello']
+
+# tuple
+t1 = (2, 5)
+
+# range
+r1 = range(10) # 0-9
+
+
+# dict
+d1 = {
+    'name': 'Jay',
+    'age': 26
+}
+
+# set
+st1 = { 'jay', 'raj', 'jay' } # no duplicate value
+
+# bool
+bool1 = True
+bool2 = False
+
+# bytes
+byt1 = b'hello'
+
+
+# Numbers
+
+# int
+# float
+# complex
+
+n1 = 1
+n2 = -1
+
+n3 = 1.0
+n4 = -1.0
+n5 = 5e4
+n6 = -2.4e6
+
+n7 = 5+3j
+n8 = 6j
+
+
+# Type conversion
+# int()
+# float()
+# complex()
+
+# String
+s1 = 'Hello'
+s2 = "Python Something"
+s3 = """Hello
+Something"""
+
+s4 = '200'
+
+print(s3)
+
+
+# type convert
+# str
+
+# Indexing
+# H e l l o
+# 0 1 2 3 4
+#-5-4-3-2-1
+
+str1 = 'Hello'
+# str1[0]
+# H
+
+# Slicing
+# H e l l o
+# 0 1 2 3 4
+str1 = 'Hello'
+# str1[0:3]
+
+
+s1 = 'hello'
+
+# String methods
+# strip
+# upper
+# lower
+# title
+# replace
+# split
+# format
+
+
+# casefold
+# count
+# endswith
+# find
+# index
+
+# isalnum
+# isalpha
+# isdecimal
+# isdigit
+# islower
+# isnumaric
+# isprintable
+# isspace
+# istitle
+# isupper
+
+
+
+# in
